719de57fd57363f0cfc961b56a3f39f12b889dd5
Justin Gery <jaygery90@gmail.com>
2023-05-19 17:15:23 -0500

    login functionality working

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,17 @@
 var express = require('express');
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 var router = express.Router();
+
+const saltRounds = 10
+
+const isAuthenticated = require('../middleware/isAuthenticated')
+
+
+
+
+
+
 
 const User = require('../models/User.model')
 
@@ -16,8 +28,13 @@ router.get('/logout', (req, res, next) => {
 //signUp
 //***creates the user */
 router.post('/signup', (req, res, next) => {
-    const { firstName, lastName, username, email, birthDate, password } = req.body
-    console.log('hahelajlkje', req.body)
+    const { username, email, password } = req.body
+    // console.log('hahelajlkje', req.body)
+
+    if (email === '' || password === '') {
+        res.status(400).json({ message: "Provide Email,Password, and Name." })
+        return
+    }
 
     // //check the password strength
     // const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
@@ -28,25 +45,59 @@ router.post('/signup', (req, res, next) => {
     //     return;
     // }
 
-    User.create({
-        firstName,
-        lastName,
-        username,
-        email,
-        birthDate,
-        password
-    })
-        .then((createdUser) => {
-            // req.session.user.createdUser
-            // console.log(createdUser)
-            res.json(createdUser)
-            //not sure how to handle this
+    User.findOne({ email })
+        .then((foundUser) => {
+            if (foundUser) {
+                res.status(400).json({ message: "User already exists." });
+                return;
+            }
+            const salt = bcryptjs.genSaltSync(saltRounds)
+            const hashedPassword = bcryptjs.hashSync(password, salt)
+
+            User.create({
+                username,
+                email,
+                // birthDate,
+                password: hashedPassword
+            })
+                .then((createdUser) => {
+                    //deconstruct new user to omit password
+                    const { email, _id, username } = createdUser
+
+                    const payload = { email, _id, username, firstName: '', lastName: '', birthDate: '' }
+
+                    const authToken = jwt.sign(
+                        payload,
+                        process.env.SECRET,
+                        { algorithm: 'HS256', expiresIn: "6h" }
+                    )
+
+                    // console.log('Signup lines', payload)
+
+                    res.status(201).json({ authToken: authToken, user: payload })
+
+                    // req.session.user.createdUser
+                    // console.log(createdUser)
+                    // res.json(createdUser)
+                    //not sure how to handle this
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.status(500).json({ message: "Internal Server Error" })
+                })
         })
         .catch((err) => {
             console.log(err)
+            res.status(401).json({ message: "user already exist!" })
         })
+
 })
 
+
+
+router.get('/verify', isAuthenticated, (req, res, next) => {
+    res.status(200).json(req.user)
+})
 
 
 module.exports = router;
